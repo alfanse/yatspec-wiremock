97b102a02a29a3cb6414d11877f4b65e68b40648
alex <alex.fanshawe@gmail.com>
2019-07-12 23:23:00 +0100

    removing duplicate annotations

--- src/test/java/com/yatspec/wiremock/YatspecWiremockReqestListenerTest.java
+++ src/test/java/com/yatspec/wiremock/YatspecWiremockReqestListenerTest.java
@@ -2,18 +2,11 @@ package com.yatspec.wiremock;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import com.googlecode.yatspec.junit.SequenceDiagramExtension;
-import com.googlecode.yatspec.junit.SpecListener;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
-@ExtendWith({
-        SpecListener.class,
-        SequenceDiagramExtension.class
-})
 class YatspecWiremockReqestListenerTest extends YatspecWiremockTests {
 
     private static WireMockServer wireMockServer = new WireMockServer(wireMockConfiguration());
@@ -30,7 +23,6 @@ class YatspecWiremockReqestListenerTest extends YatspecWiremockTests {
         reqestListener.setYatspec(testState());
         wireMockServer.addMockServiceRequestListener(reqestListener);
     }
-
 
     @Override
     WireMockServer wireMockServer() {
